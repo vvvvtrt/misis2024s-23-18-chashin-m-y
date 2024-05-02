8c712f6e5716fc47b987411b324060a00345b62a
vvvvtrt <sleim2000@gmail.com>
2024-05-02 14:41:44 +0300

    update

--- prj.cw/opencv_/main.cpp
+++ prj.cw/opencv_/main.cpp
@@ -1,13 +1,14 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <algorithm>
 #include <iostream>
 
 
 enum class Color { RED, GREEN, BLUE, WHITE, YELLOW, ORANGE };
 
 struct Circuit {
-	Circuit(int ar, cv::Point st, cv::Point en): area(ar), start(st), end(en) {}
+	Circuit(int ar, cv::Point st, cv::Point en) : area(ar), start(st), end(en) {}
 	~Circuit() = default;
 
 	int area;
@@ -21,7 +22,7 @@ public:
 	~Detected() = default;
 
 	void Snapshot(std::string& path) {
-		cv::VideoCapture cap(0); 
+		cv::VideoCapture cap(0);
 
 		if (!cap.isOpened()) {
 			std::cerr << "Error: Could not open camera" << std::endl;
@@ -53,12 +54,14 @@ public:
 			std::cout << area << std::endl;
 			std::string objectType;
 
-			if (area > 2000)
+			if (area > 5000)
 			{
-				float peri = arcLength(contours[i], true);
+				double peri = arcLength(contours[i], true);
 				approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
 				std::cout << conPoly[i].size() << std::endl;
 				boundRect[i] = boundingRect(conPoly[i]);
+
+				std::cout << "Cord " << boundRect[i].tl().x << ' ' << boundRect[i].tl() << '\n';
 
 				Circuit temp(area, boundRect[i].tl(), boundRect[i].br());
 				arr_detect.push_back(temp);
@@ -85,16 +88,38 @@ public:
 		}
 
 	}
-	
+
+	void Search_square() {
+		std::cout << "Start " << '\n';
+		std::sort(arr_detect.begin(), arr_detect.end(), [](Circuit x, Circuit y) { return x.area < y.area; });
+
+		int min_diff = pow(10, 4);
+
+		if (arr_detect.size() >= 9) {
+			for (int i = 0; i < arr_detect.size() - 9; i++) {
+				if (min_diff > arr_detect[i + 9].area - arr_detect[i].area) {
+					min_diff = arr_detect[i + 9].area - arr_detect[i].area;
+					index_detected = i;
+				}
+			}
+			std::cout << "Sort_ind " << arr_detect[index_detected].area << ' ' << index_detected << '\n';
+		}
+		else
+		{
+			throw "Not found";
+		}
+	}
 
 private:
 	std::vector<Circuit> arr_detect;
 	std::vector<Color> color_cube;
+
+	int index_detected;
 };
 
 
 int main() {
-	cv::VideoCapture cap(0); // 0 - индекс камеры
+	/*cv::VideoCapture cap(0); // 0 - индекс камеры
 
 	if (!cap.isOpened()) {
 		std::cerr << "Error: Could not open camera" << std::endl;
@@ -107,7 +132,7 @@ int main() {
 
 	// сохранение кадра в файл
 	//cv::imwrite("Resources/test.jpg", frame);
-	cap.release();
+	cap.release();*/
 
 
 	std::string path = "Resources/test.jpg";
@@ -123,6 +148,7 @@ int main() {
 
 	Detected temp;
 	temp.Recognition(imgDil, img);
+	temp.Search_square();
 
 
 	imshow("Image", img);
